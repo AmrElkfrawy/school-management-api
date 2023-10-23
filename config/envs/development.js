0896b3c449524b488b7ddd95a7f110c40a4f1ee8
Amr Elkfrawy <amrelkfrawy5@gmail.com>
2023-10-23 19:38:57 +0300

    Configure environment for production and development

--- config/envs/development.js
+++ config/envs/development.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
 module.exports = {
-    
-}
+  MONGO_URI: process.env.MONGO_URI_DEV,
+  REDIS_URI: process.env.REDIS_URI_DEV,
+};
--- config/envs/production.js
+++ config/envs/production.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
 module.exports = {
-    
-}
+  MONGO_URI: process.env.MONGO_URI_PROD,
+  REDIS_URI: process.env.REDIS_URI_PROD,
+};
